9ef18ac9c64b82c2b23ed34dcb654d9e92c00797
Morton358 <vovankovalchuk358@gmail.com>
2018-03-15 01:01:18 +0100

    created basic folder structure and create some containers and components for test if routing and lazy loading is working

---
+++ src/containers/Pizza.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import PizzaImage from '../components/Pizzaimage/Pizzaimage';
+
+class Pizza extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> The Pizza </h1>
+                <PizzaImage />
+            </div>
+        );
+    }
+
+}
+
+export default Pizza;
---
+++ src/containers/Users.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> The Users </h1>
+                <p> Awseme users here! </p>
+            </div>
+        );
+    }
+
+}
+
+export default Users;
